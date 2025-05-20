be2e6114a9a389a42642f1e985761cceb4a3821c
kafi <alkafirupam1@gmail.com>
2025-05-20 17:48:44 +0600

    add team pages

--- senior-team.php
+++ senior-team.php
@@ -1,2 +1,47 @@
 <?php require_once('header.php'); ?>
+<main>
+  <section class="team-section">
+    <div class="container">
+      <h2 class="section-title text-center">Senior Team</h2>
+      <div class="row g-4 pt-4">
+          <div class="col-3 team">
+            <a href="team.php">
+              <img src="images/4.png" alt="Team Flag">
+              <h4 class="team-name">FDF</h4>
+            </a>
+          </div>
+          <div class="col-3 team">
+            <a href="team.php">
+              <img src="images/6.png" alt="Team Flag">
+              <h4 class="team-name">Super-6</h4>
+            </a>
+          </div>
+          <div class="col-3 team">
+            <a href="team.php">
+              <img src="images/7.png" alt="Team Flag">
+              <h4 class="team-name">Friends Forever</h4>
+            </a>
+          </div>
+          <div class="col-3 team">
+            <a href="team.php">
+              <img src="images/8.png" alt="Team Flag">
+              <h4 class="team-name">Atkahon</h4>
+            </a>
+          </div>
+          <div class="col-3 team">
+            <a href="team.php">
+              <img src="images/9.png" alt="Team Flag">
+              <h4 class="team-name">Sunno-9</h4>
+            </a>
+          </div>
+          <div class="col-3 team">
+            <a href="team.php">
+              <img src="images/10.png" alt="Team Flag">
+              <h4 class="team-name">Ek-0-10</h4>
+            </a>
+          </div>
+        </div>
+    </div>
+  </section>
+</main>
 <?php require_once('footer.php'); ?>
