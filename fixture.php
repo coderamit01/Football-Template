7fa5a6f2adc81186e7b836caba7cc16f95ca7705
kafi <alkafirupam1@gmail.com>
2025-05-21 18:25:22 +0600

    add fixture page

--- fixture.php
+++ fixture.php
@@ -5,9 +5,54 @@
       <h2 class="page-title text-center mb-0">Fixture</h2>
     </div>
   </section>
-  <section class="fixture-section">
+  <section class="fixture-section py-5">
     <div class="container">
-
+      <div class="row gap-4">
+        <div class="col-md-6 fixture-card d-flex justity-content-between">
+          <div class="fixture-content gap-2">
+            <div class="fixture-team d-flex align-items-center justify-content-between">
+              <div class="d-flex align-items-center gap-2">
+                <img src="images/4.png" alt="Team Flag">
+                <span class="fx-team-name">Falcon</span>
+              </div>
+              <span class="goal">4</span>
+            </div>
+            <div class="fixture-team d-flex align-items-center justify-content-between">
+              <div class="d-flex align-items-center gap-2">
+                <img src="images/4.png" alt="Team Flag">
+                <span class="fx-team-name">Falcon</span>
+              </div>
+              <span class="goal">4</span>
+            </div>
+          </div>
+          <div class="fixture-result d-flex flex-column justify-content-center align-items-center">
+              <span>Sat, May 24</span>
+              <span>8:15 PM</span>
+          </div>
+        </div>
+        <div class="col-md-6 fixture-card d-flex justity-content-between">
+          <div class="fixture-content gap-2">
+            <div class="fixture-team d-flex align-items-center justify-content-between">
+              <div class="d-flex align-items-center gap-2">
+                <img src="images/4.png" alt="Team Flag">
+                <span class="fx-team-name">Falcon</span>
+              </div>
+              <span class="goal">4</span>
+            </div>
+            <div class="fixture-team d-flex align-items-center justify-content-between">
+              <div class="d-flex align-items-center gap-2">
+                <img src="images/4.png" alt="Team Flag">
+                <span class="fx-team-name">Falcon</span>
+              </div>
+              <span class="goal">4</span>
+            </div>
+          </div>
+          <div class="fixture-result d-flex flex-column justify-content-center align-items-center">
+              <span>Sat, May 24</span>
+              <span>8:15 PM</span>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </main>
